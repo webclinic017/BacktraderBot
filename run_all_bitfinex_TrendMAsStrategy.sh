ba6ed4dc6162050aee6e170c1ea984ee2c573960
Alexander Grinchik <a.grinchik@gmail.com>
2018-12-17 20:29:39 +0200

    Prepared for Run0007

--- run_all_bitfinex_TrendMAsStrategy.sh
+++ run_all_bitfinex_TrendMAsStrategy.sh
@@ -3,16 +3,16 @@
 declare exchange="bitfinex"
 
 #declare -a arr_symbols=("BTCUSDT"  "ETHUSDT" "XRPUSDT" "LTCUSDT" "ETCUSDT" "IOTAUSDT" "EOSUSDT" "NEOUSDT" "ZECUSDT" "ETPUSDT" "XMRUSDT" "DASHUSDT")
-declare -a arr_symbols=("BTCUSDT")
+declare -a arr_symbols=("BTCUSDT"  "ETHUSDT" "XRPUSDT" "LTCUSDT" "ETCUSDT" "IOTAUSDT" "EOSUSDT")
 
 #declare -a arr_timeframes=("30m" "1h" "3h" "6h" "12h")
-declare -a arr_timeframes=("6h")
+declare -a arr_timeframes=("15m" "30m" "1h" "3h")
 
 declare -a date_mode="Monthly" # Yearly/Monthly
 #declare -a arr_years=("2017" "2018")
 declare -a arr_years=("2018")
 #declare -a arr_months=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12")
-declare -a arr_months=("8")
+declare -a arr_months=("1" "2" "3" "4" "5" "6" "7" "8")
 declare -a arr_num_days=("31" "28" "31" "30" "31" "30" "31" "31" "30" "31" "30" "31")
 
 process_backtest() {
