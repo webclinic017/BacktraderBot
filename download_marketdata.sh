47324e35ff02f74fc0cadee0c1bd18bcc1f77faf
Alexander Grinchik <a.grinchik@gmail.com>
2019-06-29 20:05:28 +0300

    Downloaded new market data from Bitfinex. Finished Run0033

--- download_marketdata.sh
+++ download_marketdata.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 
 
-# declare -a arr_symbols=( "BTC/USDT" "ETH/USDT" "XRP/USDT" "LTC/USDT" "ETC/USDT" "IOTA/USDT" "EOS/USDT" "NEO/USDT" "ZEC/USDT" "ETP/USDT" "XMR/USDT"  "DASH/USDT")
-declare -a arr_symbols=( "BTC/USDT" "LTC/USDT" "ETH/USDT" "XRP/USDT" "ETC/USDT" "IOTA/USDT" "EOS/USDT" "NEO/USDT" "ZEC/USDT" "ETP/USDT" "XMR/USDT"  "DASH/USDT")
+#declare -a arr_symbols=( "BTC/USD" "ETH/USD" "LTC/USD" "XRP/USD" "ETC/USD" "IOTA/USD" "EOS/USD" "NEO/USD" "ZEC/USD" "ETP/USD" "XMR/USD"  "DASH/USD")
+declare -a arr_symbols=( "BTC/USD" )
+
 #declare -a arr_timeframes=("1m" "5m" "15m" "30m" "1h" "3h" "6h" "12h" "1d")
-declare -a arr_timeframes=("1m" "5m")
+declare -a arr_timeframes=("5m" "15m" "30m" "1h" "3h" "6h" "12h" "1d")
+
 
 declare exchange="bitfinex"
 
