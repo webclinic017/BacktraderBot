fc95513e72b51bd379527260a0410b4a00081616
Alexander Grinchik <a.grinchik@gmail.com>
2019-07-03 13:42:45 +0300

    Prepared for TradeRun0008

--- deploy_live.sh
+++ deploy_live.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-rsync -v -a  --exclude='.git' --exclude='.gitignore' --exclude='.idea' --exclude='*pycache*' --exclude='bot_logs' --exclude='docs' --exclude='marketdata' --exclude='strategyrun_results' --include='*' -e ssh . alex@159.69.10.75:/home/alex/BacktraderBot/
+rsync -v -a  --exclude='.git' --exclude='.gitignore' --exclude='.idea' --exclude='*pycache*' --exclude='bot_logs' --exclude='trade_runs' --exclude='docs' --exclude='marketdata' --exclude='strategyrun_results' --include='*' -e ssh . alex@159.69.10.75:/home/alex/BacktraderBot/
