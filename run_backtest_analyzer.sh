544789ea5a2ca36612a9917a66fe9b4cfc9c4cbf
Alexander Grinchik <a.grinchik@gmail.com>
2020-01-21 19:12:33 +0200

    Run0072 has completed and analyzed.

--- run_backtest_analyzer.sh
+++ run_backtest_analyzer.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-declare -a runid="Run0071"
-declare -a fname="Run0071_Step3.csv"
+declare -a runid="Run0072"
+declare -a fname="Run0072_Step3.csv"
 
 python backtest_analyzer.py -r $runid -f $fname
