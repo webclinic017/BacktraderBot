d4fadcb4b9fc8b6fa4b040ca55ac922a515f5096
Alexander Grinchik <a.grinchik@gmail.com>
2020-01-31 09:12:43 +0200

    Finished Run0070 execution. Analysis pending.

--- run_backtest_analyzer.sh
+++ run_backtest_analyzer.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-declare -a runid="Run0069"
-declare -a fname="Run0069_Step1.csv"
+declare -a runid="Run0070"
+declare -a fname="Run0070_Step1.csv"
 
 python backtest_analyzer.py -r $runid -f $fname
